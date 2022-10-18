7012878fe58c1da83f5e80e976b44014033632e6
Atayev <atayev25@gmail.com>
2022-10-18 04:17:10 +0400

    fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,7 +19,7 @@ function About() {
           <div className='col-md-5'>
               <h1 className='text-center pt-sm-5 '>About me.</h1>
                 <p className='text-center customColor'>Frontend Developer</p>
-                <p className='text-center customColor2 p-sm-3'>I am <b className='text-dark'>Ibrahim Atayev </b>Hi! I am a Frontend Developer with experience more than 1 year.I have studied Computer Science which provided me with some outstanding and technical skills. I consider myself equipped with most of web development skills, but I know I still need to learn a lot more. I am highly trainable and can easily learn tasks. If I don't have experience with software or skill, I am doing all the research to study it. I believe in hard work and honesty, I can make your job in time with high quality.</p>
+                <p className='text-center customColor2 p-sm-3'>Hi! I am a Frontend Developer with experience more than 1 year.I have studied Computer Science which provided me with some outstanding and technical skills. I consider myself equipped with most of web development skills, but I know I still need to learn a lot more. I am highly trainable and can easily learn tasks. If I don't have experience with software or skill, I am doing all the research to study it. I believe in hard work and honesty, I can make your job in time with high quality.</p>
               </div>
               <div className='col-md-5'>
                   <h1 className='text-center pt-sm-5'>My current skills.</h1>
